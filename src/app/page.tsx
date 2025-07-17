48dd78284596166e401a9be06944f8251570d1d2
Diego <pasayealvarado@gmail.com>
2025-07-16 21:03:53 -0600

    avance del html inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,102 +2,80 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      {/* SECCION DEL HEADER */}
+      <section className="header">
+        <div>
+          <p>COMUNIDAD GENERATIVA SIN FINES DE LUCRO</p>
+  
+          <div>
+            <h1>TEJIENDO</h1>
+            <h1>LAS</h1>
+            <h1>CONSCIENCIAS</h1>
+          </div>
+  
+          <p>Conectamos tradiciones ancestrales con innovación sostenible para crear un futuro regenerativo</p>
+  
+          <p>NakaWé - Madre de la Tierra</p>
+  
+          <div>
+            <button>Ver Galeria</button>
+            <button>Ver Catalogos</button>
+            <button>Contribuye</button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  
+        <Image src="/logoNakawe.png" alt="Logo Nakawe" width={170} height={50}/>
+      </section>
+
+
+      {/* SECCION DE CONTADORES */}
+      
+      <div>
+        <div>
+          <h2>15+</h2>
+          <p>Comunidades</p>
+        </div>
+
+        <div>
+          <h2>200+</h2>
+          <p>Artesanos</p>
+        </div>
+
+        <div>
+          <h2>50+</h2>
+          <p>Talleres</p>
+        </div>
+
+        <div>
+          <h2>7+</h2>
+          <p>Años</p>
+        </div>
+      </div>
+
+    {/* SECCION DE EVENTOS   */}
+
+      <div className="eventos">
+        <h2>Próximos Eventos</h2>
+
+        <div></div> {/*BARRA DEJADO DE PROXIMOS EVENTOS*/}
+
+        <p>Del 23 al 25 de Mayo</p> {/*¿¿¿Será mejor eliminarlo??? */}
+        <p>San Cristóbal de las Casas</p>
+        
+        <div>
+          <Image className="imageArrowLeft" src={'/arrow.svg'} width={50} height={50} alt="Arrow left"/>
+
+          <div className="cardEvent cardEvent1">
+
+          </div>
+
+          <Image className="imageArrowRight" src={'/arrow.svg'} width={50} height={50} alt="Arrow right"/>
+        </div>
+      </div>
+
+
+
     </div>
   );
 }
