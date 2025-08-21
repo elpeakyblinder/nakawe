61e7982d8bcef97ac14cd7a142880ec5887144cf
Diego <pasayealvarado@gmail.com>
2025-08-21 17:59:12 -0600

    admin responsivo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,16 @@ import { Montserrat as MontserratFont } from 'next/font/google';
 import './globals.css';
 import { getAuthenticatedUserProfile } from '@/lib/data';
 import Navbar from '@/components/layout/Navbar';
+import type { Metadata } from 'next'
+
 
 export const dynamic = 'force-dynamic';
 
 const montserrat = MontserratFont({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  viewport: 'width=device-width, initial-scale=1.0',
+};
 
 export default async function RootLayout({
   children,
